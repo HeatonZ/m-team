d57a2aced5c6249e17c28398f7cc2a0cfe8d870d
复读机 <905342035@qq.com>
2026-04-28 20:42:52 +0800

    delete dist

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * M-Team Plugin — 去中心化任务池协作
- * OpenClaw pluginApi: 2026.4.22
- *
- * 配置来源：openclaw.json plugins.entries.m-team.config
- */
-declare const _default: any;
-export default _default;
-//# sourceMappingURL=index.d.ts.map
